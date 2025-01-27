b3a0f9a1b1069b87c703b0f4f144ea0adc4f4edf
aryacs <aryanchandrapal@gmail.com>
2025-01-27 16:01:08 +0530

    Code added

--- Unit-I_Trees/Tree_Implementation.py
+++ Unit-I_Trees/Tree_Implementation.py
@@ -1,3 +1,24 @@
 class Node:
-    def newNode(self, value):
-        pass
+    def __init__(self, value):
+        self.left = None
+        self.data = value
+        self.right = None
+
+class Tree:
+    def createNode(self, data):
+        return Node(data)
+    def insert(self, node, data):
+        if node is None:
+            return self.createNode(data)
+        if data < node.data:
+            node.left = self.insert(node.left, data)
+        else:
+            node.right = self.insert(node.right, data)
+        return node
+
+#Driver code
+tree = Tree()
+root = tree.createNode(5)
+print("Root of Tree is:", root.data)
+
+
