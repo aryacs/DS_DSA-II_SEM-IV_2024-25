9482d02eeea29c06b7006d842316f2b6c698bd8e
aryacs <aryanchandrapal@gmail.com>
2025-02-19 14:14:28 +0530

    New File added

--- Unit-I_Trees/BinaryTree_1.1.py
+++ Unit-I_Trees/BinaryTree_1.1.py
@@ -6,11 +6,11 @@ class Node:
     def inorder(self):
         if self.left:
             self.left.inorder()
-        print(self.val, end = " ")
+        print(self.val, end = ",")
         if self.right:
             self.right.inorder()
     def preorder(self):
-        print(self.val, end=" ")
+        print(self.val, end=",")
         if self.left:
             self.left.preorder()
         if self.right:
@@ -20,14 +20,19 @@ class Node:
             self.left.postorder()
         if self.right:
             self.right.postorder()
-        print(self.val, end=" ")
+        print(self.val, end=",")
 
 root = Node(5)
 root.left = Node(10)
 root.right = Node(15)
+root.left.left = Node(23)
+root.left.right = Node(25)
 
+print("Inorder Traversal is: ", end = "")
 root.inorder()
 print()
+print("Preorder Traversal is: ", end = "")
 root.preorder()
 print()
+print("Postorder Traversal is: ", end = "")
 root.postorder()
